cc9000cb490cca9000cfaf1a3ec4fa8d883c580e
kaleseed <malongsenge@gmail.com>
2023-03-20 16:18:51 -0400

    built FrontISTR;

--- FrontISTRConfig.h
+++ FrontISTRConfig.h
@@ -5,32 +5,30 @@
 #define _FRONTISTRCONFIG_H_
 
 #define VERSION_MAJOR 5
-#define VERSION_MINOR 0
+#define VERSION_MINOR 5
 #define VERSION_PATCH 0
-#define GIT_HASH "a3fc6159b6106b96ecc9fe2b3f450e5bf74ed62c"
+#define GIT_HASH "0fe1a40f98fb0afe83df3718e65e97e67074cfb3"
+#define BUILD_DATE "3/20/2023"
 
 /* #undef _WINDOWS */
 /* #undef NDEBUG */
 /* #undef DEBUG */
 
 #define WITH_TOOLS
-#define WITH_MPI
+/* #undef WITH_MPI */
 #define WITH_OPENMP
 /* #undef WITH_REFINER */
 /* #undef WITH_REVOCAP */
 /* #undef WITH_METIS */
 /* #undef WITH_METIS_VER_4 */
 /* #undef WITH_MUMPS */
-/* #undef WITH_LAPACK */
+#define WITH_LAPACK
 /* #undef WITH_ML */
 /* #undef WITH_PARMETIS */
-/* #undef WITH_MKL */
 #define WITH_MKL
-/* #undef WITH_PARACON */
 /* #undef WITH_PARADISO */
 
-/* #undef HECMW_SERIAL */
-//#undef HECMW_SERIAL
+#define HECMW_SERIAL
 /* #undef HECMW_WITH_REFINER */
 /* #undef HECMW_WITH_METIS */
 /* #undef HECMW_PART_WITH_METIS */
--- buildFemSolver2.bat
+++ buildFemSolver2.bat
@@ -3,5 +3,7 @@ REM call ../../env_var.bat
 set WELSIM_SRC=D:\WelSimLLC-github\FrontISTR
 set WELSIM_LIBPACK=D:\WelSimLLC\CodeDV\libPack
 set WELSIM_EXEC=D:\WelSimLLC\executable26
+set INTEL_MKL=C:\Program Files (x86)\Intel\oneAPI\mkl\2022.2.0
+set IFORT_COMPILER=C:\Program Files (x86)\Intel\oneAPI\compiler\2022.2.0\windows
 
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" /run FemSolver2.sln
