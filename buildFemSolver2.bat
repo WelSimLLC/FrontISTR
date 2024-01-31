5b19c2b74e96393da482905b60985833697d9fe7
kaleseed <kaleseed@google.com>
2024-01-30 21:40:19 -0500

    upgrade MKL and Fortran Compiler to 2024.0

--- buildFemSolver2.bat
+++ buildFemSolver2.bat
@@ -1,7 +1,7 @@
 set WELSIM_SRC=D:\WelSimLLC\CodeDV\WelSim\v28\src
 set WELSIM_EXEC=D:\WelSimLLC\executable28
-set INTEL_MKL=C:\Program Files (x86)\Intel\oneAPI\mkl\2022.2.0
-set IFORT_COMPILER=C:\Program Files (x86)\Intel\oneAPI\compiler\2022.2.0\windows
+set INTEL_MKL=C:\Program Files (x86)\Intel\oneAPI\mkl\latest
+set IFORT_COMPILER=C:\Program Files (x86)\Intel\oneAPI\compiler\latest
 
 set WELSIM_LIBPACK=D:\WelSimLLC\CodeDV\libPack
 set FrontISTR_SRC=D:\WelSimLLC-github\FrontISTR
