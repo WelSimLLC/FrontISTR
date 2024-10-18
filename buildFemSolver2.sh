c86cec6995a8a6c3b06ade9cb225df458e6058d8
kaleseed <kaleseed@g.com>
2024-10-18 01:03:29 -0400

    build from Linux;

--- buildFemSolver2.sh
+++ buildFemSolver2.sh
@@ -3,8 +3,8 @@
 export FRONTISTR_SRC=$HOME/WelSimLLC-github/FrontISTR
 export WELSIM_LIBPACK=$HOME/WelSimLLC/CodeDV/libPack/Linux
 export WELSIM_EXEC=$HOME/WelSimLLC/executable30
-export INTEL_DIR=$HOME/mySys/intel/oneapi/compiler/2022.1.0/linux
-export MKL_DIR=$HOME/mySys/intel/oneapi/mkl/2022.1.0
+export INTEL_DIR=$HOME/mySys/intel/oneapi/compiler/2024.2
+export MKL_DIR=$HOME/mySys/intel/oneapi/mkl/2024.2
 
 
 echo "Run CodeBlocks..."
